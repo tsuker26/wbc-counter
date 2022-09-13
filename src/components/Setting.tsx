c0fc9a269e2d0b8a7974244d7fd2113f8aea8f5c
igor <igorek888rane@gmail.com>
2022-09-13 16:16:42 +0300

    fix

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -7,25 +7,30 @@ type SettingProps = {
 
 const Setting: FC<SettingProps> = ({clear}) => {
 
-    const {mode, setMode, cells, setCells, wbc, setWbc, maxCount, setMaxCount} = useMainContext()
+    const {mode, setMode, cells, setCells, setWbc, maxCount, setMaxCount} = useMainContext()
 
     const [add, setAdd] = useState<string>('')
+    const [inputWbc, setInputWbc] = useState<string>('')
     const addCell = () => {
         if (add) {
             setCells([...cells, add])
             setAdd('')
         }
     }
+    const changeWbc = (num: string) => {
+        setInputWbc(num)
+        setWbc(+inputWbc)
+    }
     return (
         <div className={'setting'}>
             <div className={'input input_wbc'}>
-                <input type="number"
-                       value={wbc}
-                       onChange={((e: ChangeEvent<HTMLInputElement>) => setWbc(+e.target.value))}/>
+                <input type="text"
+                       value={inputWbc}
+                       onChange={((e: ChangeEvent<HTMLInputElement>) => changeWbc(e.target.value))}/>
                 <span>10⁹/L</span>
             </div>
             <div className={'input max_count'}>
-                <input type="number"
+                <input type="tel"
                        value={maxCount}
                        onChange={((e: ChangeEvent<HTMLInputElement>) => setMaxCount(+e.target.value))}/>
                 <span>Max</span>
