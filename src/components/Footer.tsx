c3b50c8680a16598d810c27fc530c7901500302a
Igor Tsuker <igorek888rane@gmail.com>
2023-01-13 18:39:55 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className={'footer'}>
-            <span>©Tsuker Corp 2022</span>
+            <span>©Tsuker Corp {new Date().getFullYear()}</span>
         </footer>
     );
 };
